33a35a867df121c451597858c2a86793871477c8
explicitworkload <peterchenwy@gmail.com>
2023-10-17 07:29:00 +0800

    bb

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ except ValueError:
 
 num1 = get_num("First value: ")
 num2 = get_num("Second value: ")
+
+
+# try:
+#     num1, num2 = map(int, input("\nProvide your 2 values: ").split())
+# except ValueError:
+#         print("Invalid input. Please enter only two integers.")
+
                  
 if selection == 1:
   value = add(num1, num2)
